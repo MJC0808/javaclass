f177f83779d39f9c150d61d44414e9f1e5623627
MJC <mjc08081234@gmail.com>
2024-03-21 14:02:10 +0900

    Even or Odd

--- src/Hello.java
+++ src/Hello.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
 
 public class Hello {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World! 12 ");
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("정수를 입력하시오 : "); // Prompt the user to enter an integer
+        int number = input.nextInt(); // Read the integer input from the user
 
-	}
+        if (number % 2 == 0) {
+            System.out.println("짝수입니다."); // Print if the number is even
+        } else {
+            System.out.println("홀수입니다."); // Print if the number is odd
+        }
 
+        input.close(); // Close the Scanner object to avoid resource leak
+    }
 }
